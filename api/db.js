a2466a3d5ee54fa94ef0a404fc80b293f8cec7f1
Sam Hurst <sam@reachboarding.co.uk>
2025-03-21 18:48:55 +0000

    Updated AWS credentials handling and added .env to .gitignore

--- api/db.js
+++ api/db.js
@@ -1,11 +1,15 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocument, PutCommand } from '@aws-sdk/lib-dynamodb';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const client = new DynamoDBClient({
     region: 'eu-west-2',
     credentials: {
-        accessKeyId: '',
-        secretAccessKey: '',
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     },
 });
 
